d30575ae1b03db4d646400826962455198bce606
Aman Sayyad <work.amansayyad@gmail.com>
2024-09-03 16:10:46 +0530

    basic set up

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose =require("mongoose")
+
+const MONGO_URL ="mongodb://127.0.0.1:27017/RentNext" //connecting db------------------//
+
+main().then(()=>{
+    console.log("connected to DB")
+
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect( MONGO_URL);
+}                                                       //-------------------//
+
+
+app.get("/",(req,res)=>{
+    res.send("iam root")
+})
+
+app.listen(8080, ()=>{
+
+    console.log("server is listning to port:8080");
+})
