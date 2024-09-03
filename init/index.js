f49ff19a32efdc78fb95f40be66a3faa8ac4ae75
Aman Sayyad <work.amansayyad@gmail.com>
2024-09-03 17:02:47 +0530

    db added

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose =require("mongoose");
+const initData =require("./data.js");
+const Listing =require("../models/listing.js");
+
+const MONGO_URL ="mongodb://127.0.0.1:27017/RentNext" //connecting db------------------//
+
+main().then(()=>{
+    console.log("connected to DB")
+
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect( MONGO_URL);
+}                                                       //-------------------//
+
+
+const initDB= async ()=>{
+   await Listing.deleteMany({}); //it will delete all data in collection 
+   await Listing.insertMany(initData.data); //insert data "initData"
+   console.log("data was initalized");
+};
+initDB();
